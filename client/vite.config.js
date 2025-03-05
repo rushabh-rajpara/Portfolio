74d9c1afa343d4b8d47e31aa288a7af76633ccf8
rushabh-2833 <rushabh4478@gmail.com>
2025-03-05 00:07:16 -0500

    github deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: './',  // Ensure this is correct
+  base: "/Portfolio/",
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000kB
+  }, // 🔹 Replace with your GitHub repository name
 });
+
